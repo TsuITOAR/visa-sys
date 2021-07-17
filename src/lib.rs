7c872c6629c9073a6d0514c9f9e9c57051ffa275
TsuITOAR <itoar@outlook.com>
2021-07-17 23:30:28 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,19 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem;
+    #[test]
+    fn open_default_RM() {
+        unsafe {
+            let session: ViSession;
+            assert_eq!(viOpenDefaultRM(&session as ViPSession) < VI_SUCCESS, false);
+        }
+    }
+}
